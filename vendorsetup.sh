a2ab296b6aca2c44e9b8e0b8c04b1ff851ada069
Sonal Singh <sonal.singh.19993@gmail.com>
2021-05-20 18:32:06 +0000

    sdm660-common: arrow => havoc
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,2 @@
+rm -rf hardware/qcom-caf/msm8998/display
+git clone https://github.com/SALMANKHANPM/hardware_qcom-caf_msm8998_display.git -b lineage-18.1-caf-msm8998 hardware/qcom-caf/msm8998/display
