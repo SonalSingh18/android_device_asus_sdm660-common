eb249a32b739f09e497577b9e6160f772b3ad275
Sonal Singh <sonal.singh.19993@gmail.com>
2021-05-21 11:29:51 +0000

    sdm660-common: havoc => xtended
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -263,7 +263,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-havoc
+    $(LOCAL_PATH)/overlay-xtended
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
