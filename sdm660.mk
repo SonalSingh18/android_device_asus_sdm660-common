d68330b842c9e6f2c81fab0a96644236a41ba896
Sonal Singh <sonal.singh.19993@gmail.com>
2021-06-19 14:34:16 +0530

    sdm660-common: xtended => dot
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -263,7 +263,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-xtended
+    $(LOCAL_PATH)/overlay-dot
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
